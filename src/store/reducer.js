ad152f9a34ead7904932c9ba1940067a0d655e1d
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-31 14:52:54 -0500

    successfully fetch data from API and stored in redux state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,15 @@
 const initialState = {
-    breed: "corgi",
-    photo: "image"
+    breeds: [],
+    selectedBreed: "Pembroke",
+    image: ""
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type){
-    case "CHANGE_BREED":
-      return {...state, breed: action.payload}
+    case "CHANGE_SELECTED_BREED":
+      return {...state, selectedBreed: action.payload}
+    case "UPDATE_BREED_LIST":
+      return {...state, breeds: action.payload}
     default:
       return state
   }
